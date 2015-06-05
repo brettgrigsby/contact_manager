665e0e06ea7f78a4caa6d69c6f71641c193ff2cd
brettgrigsby <gbrett@gmail.com>
2015-06-04 21:25:46 -0600

    email controller tests passing

--- spec/controllers/email_addresses_controller_spec.rb
+++ spec/controllers/email_addresses_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe EmailAddressesController, type: :controller do
   # EmailAddress. As you add validations to EmailAddress, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {address: 'test@rspec.com', person_id: 1}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {address: nil, person_id: nil}
   }
 
   # This should return the minimal set of values that should be in the session
@@ -102,15 +102,18 @@ RSpec.describe EmailAddressesController, type: :controller do
 
   describe "PUT #update" do
     context "with valid params" do
+      let(:bob) { Person.create(first_name: 'Bob', last_name: 'Smith')}
+      let(:valid_attributes) { {address: 'test@rspec.com', person_id: bob.id} }
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        {address: 'new@address.com', person_id: bob.id}
       }
 
       it "updates the requested email_address" do
         email_address = EmailAddress.create! valid_attributes
         put :update, {:id => email_address.to_param, :email_address => new_attributes}, valid_session
         email_address.reload
-        skip("Add assertions for updated state")
+        expect(email_address.address).to eq('new@address.com')
+        expect(email_address.person_id).to eq(bob.id)
       end
 
       it "assigns the requested email_address as @email_address" do
