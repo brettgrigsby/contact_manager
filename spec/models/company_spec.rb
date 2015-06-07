21703f4691c0d1a6cfa49a59dd70f46e4670e16b
brettgrigsby <gbrett@gmail.com>
2015-06-07 13:43:28 -0600

    added companies

---
+++ spec/models/company_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Company, type: :model do
+  let(:company) { Company.new(name: 'Acme') }
+
+  it "is valid" do
+    expect(company).to be_valid
+  end
+
+  it "is invalid without a name" do
+    company.name = nil
+    expect(company).not_to be_valid
+  end
+
+  it "has an array of associated phone numbers" do
+    expect(company.phone_numbers).to eq([])
+  end
+end
